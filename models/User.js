fe3d2445c0be089f0b886edad828b6cae965388e
eternaleight <eternaleight0@gmail.com>
2022-06-01 20:29:13 +0900

    feat(mongodb): add db schema

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 25,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    profilePicuture: {
+      type: String,
+      default: "",
+    },
+    coverPicuture: {
+      type: String,
+      defalut: "",
+    },
+    followers: {
+      type: Array,
+      defalut: [],
+    },
+    followings: {
+      type: Array,
+      defalut: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 70,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+  },
+
+  { timestamps: true }
+)

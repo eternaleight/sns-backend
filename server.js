b7bafef957d04aafea397ac4d98650638f517461
eternaleight <eternaleight0@gmail.com>
2022-06-01 19:54:39 +0900

    feat(auth & posts): add middleware

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const userRoute = require("./routes/users")
+const postsRoute = require("./routes/posts")
+const authRoute = require("./routes/auth")
 
 //ミドルウェア
 app.use("/api/users", userRoute)
+app.use("/api/posts", postsRoute)
+app.use("/api/auth", authRoute)
 
 app.get("/",(req,res) => {
   res.send("hello express")

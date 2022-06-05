9f5cd620669afcdf5200757c8fe65335d741b2b1
eternaleight <eternaleight0@gmail.com>
2022-06-05 22:36:39 +0900

    feat(post): add update post features

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,22 @@ router.post("/", async (req, res) => {
   }
 })
 
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      })
+      return res.status(200).json("投稿の編集に成功しました!")
+    } else {
+      return res.status(403).json("他の人の投稿は編集できません")
+    }
+  } catch (err) {
+    return res.status(403).json(err)
+  }
+})
 
 
 module.exports = router
